35fd8e7fb0eaba59e7645698f18027d8f793458c
harrisonBirkner <57458202+harrisonBirkner@users.noreply.github.com>
2019-12-18 10:29:07 -0600

    CBLHJB02 DAT file and program finished and ran. PRT file created for program output. Ready to have Mark review.

--- CBLHJB02/CBLHJB02.cbl
+++ CBLHJB02/CBLHJB02.cbl
@@ -26,7 +26,7 @@
 			   10 I-PURCHASE-MM           PIC 99.
 			   10 I-PURCHASE-DD           PIC 99.
 		   05 I-BOAT-TYPE                 PIC X.
-		   05 I-ACCESSORY-PACKAGE         PIC 9.
+		   05 I-ACC-PACK         PIC 9.
 		   05 I-PREP-COST                 PIC 9(5)V99.
 
 	   FD PRTOUT
@@ -40,13 +40,22 @@
        WORKING-STORAGE SECTION.
 	   01 MISC.
 		   05 H-BOAT-TYPE                 PIC X.
-		   05 MORE-RECS                  PIC X(3)    VALUE 'YES'.
-		   05 PAGE-CTR                   PIC 99      VALUE 0.
+		   05 MORE-RECS                   PIC X(3)    VALUE 'YES'.
+		   05 PAGE-CTR                    PIC 99      VALUE 0.
 		   05 CURRENT-DATE-AND-TIME.
 			   10 CURRENT-YEAR            PIC X(4).
 			   10 CURRENT-MONTH           PIC XX.
 			   10 CURRENT-DAY             PIC XX.
 			   10 CURRENT-TIME            PIC X(11).
+		   05 CALCS.
+			   10 C-TOTAL-COST            PIC 9(10)V99.
+
+		   05 MJ-SUBTOTALS.
+			   10 C-MJ-NUM-SALES          PIC 9(6)      VALUE 0.
+			   10 C-MJ-TOTAL-SALES        PIC 9(11)V99  VALUE 0.
+		   05 GRAND-TOTALS.
+			   10 C-GT-NUM-SALES          PIC 9(7)      VALUE 0.
+			   10 C-GT-TOTAL-SALES        PIC 9(13)V99  VALUE 0.
 
 	   01 TITLE-LINE.
 	       05 FILLER                      PIC X(6)      VALUE 'DATE: '.
@@ -63,16 +72,85 @@
 		   05 FILLER                      PIC X(6)      VALUE 'PAGE: '.
 		   05 TITLE-PAGE                  PIC Z9.
 
+	   01 COL-HEADING1.
+		   05 FILLER                      PIC X(8)
+		      VALUE 'CUSTOMER'.
+		   05 FILLER                      PIC X(36)     VALUE SPACES.
+		   05 FILLER                      PIC X(4)      VALUE 'BOAT'.
+		   05 FILLER                      PIC X(9)      VALUE SPACES.
+		   05 FILLER                      PIC X(8)
+              VALUE 'PURCHASE'.
+		   05 FILLER                      PIC X(11)     VALUE SPACES.
+		   05 FILLER                      PIC X(9)
+              VALUE 'ACCESSORY'.
+		   05 FILLER                      PIC X(21)     VALUE SPACES.
+		   05 FILLER                      PIC X(4)      VALUE 'PREP'.
+		   05 FILLER                      PIC X(17)     VALUE SPACES.
+		   05 FILLER                      PIC X(5)      VALUE 'TOTAL'.
+
+	   01 COL-HEADING2.
+		   05 FILLER                      PIC X(9)
+              VALUE 'LAST NAME'.
+		   05 FILLER                      PIC X(14)     VALUE SPACES.
+		   05 FILLER                      PIC X(5)      VALUE 'STATE'.
+		   05 FILLER                      PIC X(16)     VALUE SPACES.
+		   05 FILLER                      PIC X(4)      VALUE 'COST'.
+		   05 FILLER                      PIC X(9)      VALUE SPACES.
+		   05 FILLER                      PIC X(4)      VALUE 'DATE'.
+		   05 FILLER                      PIC X(15)     VALUE SPACES.
+		   05 FILLER                      PIC X(7)
+              VALUE 'PACKAGE'.
+		   05 FILLER                      PIC X(23)     VALUE SPACES.
+		   05 FILLER                      PIC X(4)      VALUE 'COST'.
+		   05 FILLER                      PIC X(18)     VALUE SPACES.
+		   05 FILLER                      PIC X(4)      VALUE 'COST'.
+
+	   01 DETAIL-LINE.
+		   05 D-LAST-NAME                 PIC X(15).
+		   05 FILLER                      PIC X(9)      VALUE SPACES.
+		   05 D-STATE                     PIC XX.
+		   05 FILLER                      PIC X(12)     VALUE SPACES.
+		   05 D-BOAT-COST                 PIC ZZZ,ZZZ.99.
+		   05 FILLER                      PIC X(9)      VALUE SPACES.
+		   05 D-PURCHASE-MM               PIC XX.
+		   05 FILLER                      PIC X         VALUE '/'.
+		   05 D-PURCHASE-DD               PIC XX.
+		   05 FILLER                      PIC X         VALUE '/'.
+		   05 D-PURCHASE-YY               PIC 99.
+		   05 FILLER                      PIC X(11)     VALUE SPACES.
+		   05 D-ACC-PACK                  PIC X(15).
+		   05 FILLER                      PIC X(10)     VALUE SPACES.
+		   05 D-PREP-COST                 PIC ZZ,ZZZ.99.
+		   05 FILLER                      PIC X(10)     VALUE SPACES.
+		   05 D-TOTAL-COST                PIC Z,ZZZ,ZZZ.99.
+
 	   01 BOAT-LINE.
 	       05 FILLER                      PIC X(11)
               VALUE 'BOAT TYPE: '.
-	       05 COL-BOAT-TYPE                 PIC X(13).
+	       05 COL-BOAT-TYPE               PIC X(13).
 
-	   01 MJ-SUBTOTAL-LINE.
-		   05 FILLER                      PIC X(23)     VALUE SPACES.
-		   05 FILLER                      PIC X(14)
+       01 MJ-SUBTOTAL-LINE.
+           05 FILLER                      PIC X(23)     VALUE SPACES.
+           05 FILLER                      PIC X(14)
               VALUE 'SUBTOTALS FOR '.
-		   05 MJ-BOAT-TYPE                PIC X(13).
+           05 MJ-BOAT-TYPE                PIC X(13).
+           05 FILLER                      PIC X(10)     VALUE SPACES.
+           05 FILLER                      PIC X(14)
+              VALUE 'NUMBER SOLD:  '.
+           05 MJ-NUM-SALES                PIC Z,ZZ9.
+           05 FILLER                      PIC X(38)     VALUE SPACES.
+           05 MJ-TOTAL-SALES              PIC $,$$$,$$$,$$$.99.
+
+	   01 TOTAL-LINE.
+		   05 FILLER                      PIC X(23)     VALUE SPACES.
+		   05 FILLER                      PIC X(12)
+              VALUE 'GRAND TOTALS'.
+		   05 FILLER                      PIC X(25)     VALUE SPACES.
+		   05 FILLER                      PIC X(13)
+		      VALUE 'NUMBER SOLD: '.
+		   05 GT-NUM-SALES                PIC ZZ,ZZ9.
+		   05 FILLER                      PIC X(35)     VALUE SPACES.
+		   05 GT-TOTAL-SALES              PIC $$$,$$$,$$$,$$$.99.
 
        PROCEDURE DIVISION.
 	   L1-MAIN.
@@ -96,11 +174,11 @@
 
        L2-MAINLINE.
 	       IF I-BOAT-TYPE NOT = H-BOAT-TYPE
-		       PERFORM L3-MJ-SUBTOTALS.
+		       PERFORM L3-MJ-SUBTOTALS
+			   PERFORM L5-EVAL-BOAT-TYPE
 		       WRITE PRTLINE FROM BOAT-LINE
-			       AFTER ADVANCING 2 LINES.
+			       AFTER ADVANCING 2 LINES
 		       WRITE PRTLINE FROM SPACES.
-		       PERFORM L5-EVAL-BOAT-TYPE.
            PERFORM L3-CALCS.
            PERFORM L3-MOVE-PRINT.
            PERFORM L3-READ-INPUT.
@@ -113,7 +191,7 @@
 
        L3-INIT-HEADING.
            ADD 1 TO PAGE-CTR.
-           MOVE PAGE-CTR                TO TITLE-PAGE.
+           MOVE PAGE-CTR                  TO TITLE-PAGE.
            WRITE PRTLINE FROM TITLE-LINE.
            WRITE PRTLINE FROM COL-HEADING1
 	           AFTER ADVANCING 2 LINES.
@@ -126,7 +204,7 @@
        L3-READ-INPUT.
            READ BOAT-INPUT
 	           AT END
-		           MOVE 'NO' TO MORE-RECS.
+		           MOVE 'NO'              TO MORE-RECS.
 			
        L3-CALCS.
 	       COMPUTE C-TOTAL-COST = I-BOAT-COST + I-PREP-COST.
@@ -135,30 +213,52 @@
 	       COMPUTE C-MJ-TOTAL-SALES = C-MJ-TOTAL-SALES + C-TOTAL-COST.
 
        L3-MOVE-PRINT.
-	       MOVE I AND C FIELDS          TO D FIELDS.
+		   EVALUATE I-ACC-PACK
+	           WHEN '1'
+		           MOVE 'ELECTRONICS'     TO D-ACC-PACK
+		       WHEN '2'
+		           MOVE 'SKI PACKAGE'     TO D-ACC-PACK
+		       WHEN '3'
+			       MOVE 'FISHING PACKAGE' TO D-ACC-PACK
+	       END-EVALUATE.
+	       MOVE I-LAST-NAME               TO D-LAST-NAME.
+		   MOVE I-STATE                   TO D-STATE.
+		   MOVE I-BOAT-COST               TO D-BOAT-COST.
+		   MOVE I-PURCHASE-DD             TO D-PURCHASE-DD.
+		   MOVE I-PURCHASE-MM             TO D-PURCHASE-MM.
+		   MOVE I-PURCHASE-YY             TO D-PURCHASE-YY.
+		   MOVE I-PREP-COST               TO D-PREP-COST.
+		   MOVE C-TOTAL-COST              TO D-TOTAL-COST.
 	       WRITE PRTLINE FROM DETAIL-LINE
 	           AFTER ADVANCING 1 LINE
 		           AT EOP
 		               PERFORM L4-HEADING.
 			
        L3-MJ-SUBTOTALS.
-	       MOVE C-MJ FIELDS             TO MJ FIELDS.
+           MOVE C-MJ-NUM-SALES            TO MJ-NUM-SALES.
+		   MOVE C-MJ-TOTAL-SALES          TO MJ-TOTAL-SALES.
 	       WRITE PRTLINE FROM MJ-SUBTOTAL-LINE
 		       AFTER ADVANCING 2 LINES
 			       AT EOP
 				       PERFORM L4-HEADING.
-	       ADD C-MJ FIELDS TO C-GT FIELDS.
-	       C-MJ FIELDS = 0.
-	       MOVE I-BOAT-TYPE             TO H-BOAT-TYPE.
+           ADD C-MJ-NUM-SALES             TO C-GT-NUM-SALES.
+		   ADD C-MJ-TOTAL-SALES           TO C-GT-TOTAL-SALES.
+
+           MOVE 0                         TO C-MJ-NUM-SALES.
+		   MOVE 0                         TO C-MJ-TOTAL-SALES.
+
+	       MOVE I-BOAT-TYPE               TO H-BOAT-TYPE.
 
        L3-TOTALS.
-	       MOVE C-GT FIELDS             TO GT FIELDS.
+           MOVE C-GT-NUM-SALES            TO GT-NUM-SALES.
+		   MOVE C-GT-TOTAL-SALES          TO GT-TOTAL-SALES.
+		   S
 	       WRITE PRTLINE FROM TOTAL-LINE
 		       AFTER ADVANCING 3 LINES.
 
        L4-HEADING.
 	       ADD 1 TO PAGE-CTR.
-	           MOVE PAGE-CTR            TO TITLE-PAGE.
+	           MOVE PAGE-CTR              TO TITLE-PAGE.
 	       WRITE PRTLINE FROM TITLE-LINE
 		       AFTER ADVANCING PAGE.
 	       WRITE PRTLINE FROM COL-HEADING1
@@ -172,23 +272,23 @@
 	   L5-EVAL-BOAT-TYPE.
 	       EVALUATE I-BOAT-TYPE
 	           WHEN 'B'
-		           MOVE 'BASS BOAT'     TO COL-BOAT-TYPE
-			       MOVE 'BASS BOAT'     TO MJ-BOAT-TYPE
+		           MOVE 'BASS BOAT'       TO COL-BOAT-TYPE
+			       MOVE 'BASS BOAT'       TO MJ-BOAT-TYPE
 		       WHEN 'P'
-		           MOVE 'PONTOON'       TO COL-BOAT-TYPE
-			       MOVE 'PONTOON'       TO MJ-BOAT-TYPE
+		           MOVE 'PONTOON'         TO COL-BOAT-TYPE
+			       MOVE 'PONTOON'         TO MJ-BOAT-TYPE
 		       WHEN 'S'
-			       MOVE 'SKI BOAT'      TO COL-BOAT-TYPE
-			       MOVE 'SKI BOAT'      TO MJ-BOAT-TYPE
+			       MOVE 'SKI BOAT'        TO COL-BOAT-TYPE
+			       MOVE 'SKI BOAT'        TO MJ-BOAT-TYPE
 		       WHEN 'J'
-			       MOVE 'JOHN BOAT'     TO COL-BOAT-TYPE
-			       MOVE 'JOHN BOAT'     TO MJ-BOAT-TYPE
+			       MOVE 'JOHN BOAT'       TO COL-BOAT-TYPE
+			       MOVE 'JOHN BOAT'       TO MJ-BOAT-TYPE
 		       WHEN 'C'
-			       MOVE 'CANOE'         TO COL-BOAT-TYPE
-			       MOVE 'CANOE'         TO MJ-BOAT-TYPE
+			       MOVE 'CANOE'           TO COL-BOAT-TYPE
+			       MOVE 'CANOE'           TO MJ-BOAT-TYPE
 		       WHEN 'R'
-			       MOVE 'CABIN CRUISER' TO COL-BOAT-TYPE
-			       MOVE 'CABIN CRUISER' TO MJ-BOAT-TYPE
+			       MOVE 'CABIN CRUISER'   TO COL-BOAT-TYPE
+			       MOVE 'CABIN CRUISER'   TO MJ-BOAT-TYPE
 	       END-EVALUATE.
            
        END PROGRAM CBLHJB02.
