f86ddfc033ad06ff2a4e6e8d8746ae671955c25a
harrisonBirkner <57458202+harrisonBirkner@users.noreply.github.com>
2020-02-16 20:22:00 -0600

    added all tables. partial calcs added for tables

--- CBLHJB06/CBLHJB06.cbl
+++ CBLHJB06/CBLHJB06.cbl
@@ -56,6 +56,7 @@
 	 
        WORKING-STORAGE SECTION.
 	   01 MISC.
+		   05 SUB                     PIC 9       VALUE 0.
 	       05 MORE-RECS               PIC X       VALUE 'Y'.
 		   05 ERR-SW                  PIC X       VALUE 'N'.
 		   05 PAGE-CTR                PIC 99      VALUE 0.
@@ -71,19 +72,6 @@
 			   10 C-TOT-SALES         PIC 9(6)V99.
 		   05 GRAND-TOTALS.
 			   10 C-GT-ERR-CTR        PIC 9(6)       VALUE 0.
-			   10 POP-ACCUMS.
-				   15 C-GT-COKE-CASES    PIC 9(8)    VALUE 0.
-				   15 C-GT-DC-CASES      PIC 9(8)    VALUE 0.
-				   15 C-GT-MY-CASES      PIC 9(8)    VALUE 0.
-				   15 C-GT-CC-CASES      PIC 9(8)    VALUE 0.
-				   15 C-GT-DCC-CASES     PIC 9(8)    VALUE 0.
-				   15 C-GT-SPRITE-CASES  PIC 9(8)    VALUE 0.
-			   10 TEAM-ACCUMS.
-				   15 C-GT-A-TEAM        PIC 9(11)V99  VALUE 0.
-				   15 C-GT-B-TEAM        PIC 9(11)V99  VALUE 0.
-				   15 C-GT-C-TEAM        PIC 9(11)V99  VALUE 0.
-				   15 C-GT-D-TEAM        PIC 9(11)V99  VALUE 0.
-				   15 C-GT-E-TEAM        PIC 9(11)V99  VALUE 0.
 
 	   01 ERRORS.
 		   05 FILLER                  PIC X(60)
@@ -105,16 +93,18 @@
                VALUE 'NUMBER OF CASES MUST BE NUMERIC'.
 		   05 FILLER                  PIC X(60)
 		       VALUE 'MINIMUM OF ONE CASE'.
+		   05 FILLER                  PIC X(60)
+               VALUE 'TEAM MUST BE A-E'.
 
 	   01 ERROR-TABLE REDEFINES ERRORS.
-	       05 ERRORS		          PIC 9(6)	  OCCURS 10 TIMES.
+	       05 ERROR-TABLE-MSG         PIC X(60)	  OCCURS 11 TIMES.
 
 	   01 TEAMS.
-           05 FILLER		          PIC X(12)	  VALUE 'A0000000000'.
-	       05 FILLER		          PIC X(12)	  VALUE 'B0000000000'.
-	       05 FILLER		          PIC X(12)	  VALUE 'C0000000000'.
-	       05 FILLER		          PIC X(12)	  VALUE 'D0000000000'.
-	       05 FILLER		          PIC X(12)	  VALUE 'E0000000000'.
+           05 FILLER		          PIC X(12)	  VALUE 'A00000000000'.
+	       05 FILLER		          PIC X(12)	  VALUE 'B00000000000'.
+	       05 FILLER		          PIC X(12)	  VALUE 'C00000000000'.
+	       05 FILLER		          PIC X(12)	  VALUE 'D00000000000'.
+	       05 FILLER		          PIC X(12)	  VALUE 'E00000000000'.
 	
        01 C-TEAM-TABLE REDEFINES TEAMS.
            05 C-TEAM-INFO                         OCCURS 5 TIMES.
@@ -253,7 +243,7 @@
 	   01 GT-POP-LINE2.   
 	       05 FILLER	              PIC XXX	  VALUE SPACES.
 	       05 GT-POP-TOTAL-TABLE.
-               10 GT-POP-TOTAL                    OCCURS 3 TIMES.
+               10 GT-POP-INFO                     OCCURS 3 TIMES.
 		           15 GT-POP-LITERAL  PIC X(16).
 		           15 FILLER		  PIC X		  VALUE SPACES.
 		           15 GT-POP-TOTAL	  PIC ZZZ,ZZ9.
@@ -265,13 +255,10 @@
        01 GT-TEAM-LINE2.
 		   05 FILLER                  PIC XXX     VALUE SPACES.
 		   05 GT-TEAM-TOTAL-TABLE.
-			   10 GT-TEAM-TOTAL                   OCCURS 1 TIMES.
+			   10 GT-TEAM-TOTAL-INFO              OCCURS 1 TIMES.
 				   15 GT-TEAM-NAME    PIC X.
 				   15 FILLER          PIC X.
 				   15 GT-TEAM-TOTAL   PIC $$$$,$$$,$$$.99.
-
-		   05 GT-TEAM-NAME            PIC X.
-		   05 GT-TEAM-TOTAL           PIC $$$$,$$$,$$$.99.
 
 	   01 ERR-TOTAL-LINE.
 		   05 FILLLER                 PIC X(13)   VALUE 'TOTAL ERRORS '.
@@ -289,23 +276,25 @@
            OPEN INPUT POP-INPUT.
            OPEN OUTPUT PRTOUT.
 	       OPEN OUTPUT ERROUT.
-           MOVE FUNCTION CURRENT-DATE        TO CURRENT-DATE-AND-TIME.
-           MOVE CURRENT-MONTH                TO TITLE-MONTH.
-           MOVE CURRENT-DAY                  TO TITLE-DAY.
-           MOVE CURRENT-YEAR                 TO TITLE-YEAR.
-	       MOVE CURRENT-MONTH                TO ERR-TITLE-MONTH.
-           MOVE CURRENT-DAY                  TO ERR-TITLE-DAY.
-           MOVE CURRENT-YEAR                 TO ERR-TITLE-YEAR.
+           MOVE FUNCTION CURRENT-DATE          TO CURRENT-DATE-AND-TIME.
+           MOVE CURRENT-MONTH                  TO TITLE-MONTH.
+           MOVE CURRENT-DAY                    TO TITLE-DAY.
+           MOVE CURRENT-YEAR                   TO TITLE-YEAR.
+	       MOVE CURRENT-MONTH                  TO ERR-TITLE-MONTH.
+           MOVE CURRENT-DAY                    TO ERR-TITLE-DAY.
+           MOVE CURRENT-YEAR                   TO ERR-TITLE-YEAR.
      
            PERFORM L3-INIT-HEADING.
            PERFORM L9-READ-INPUT.
 
 		   INITIALIZE GT-POP-TOTAL-TABLE.
 		   INITIALIZE C-POP-TOTAL-TABLE.
+		   INITIALIZE GT-TEAM-TOTAL-TABLE.
+		   INITIALIZE C-TEAM-TABLE.
 
        L2-MAINLINE.
 	       PERFORM L3-VALIDATION
-		       THRU L3-VALIDATION-EXIT.
+		       THRU L9-VALIDATION-EXIT.
 	       IF ERR-SW = 'N'
 		       PERFORM L3-CALCS
 		       PERFORM L3-MOVE-PRINT
@@ -324,9 +313,10 @@
        L3-INIT-HEADING.
       *THIS PARAGRAPH FUNCTIONS SIMILARLY TO L4-HEADING, EXCEPT IT IS
       *ONLY RUN ONCE AT THE BEGINNING OF THE PROGRAM, DOES NOT PRINT
-      *AN EXTRA 60 BLANK LINES, AND PRINTS INIT HEADINGS FOR ERROR REPORT.
-           ADD 1 TO PAGE-CTR.
-           MOVE PAGE-CTR TO TITLE-PAGE.
+      *AN EXTRA 60 BLANK LINES, AND PRINTS INIT HEADINGS FOR ERROR
+      *REPORT.                                                          
+           ADD 1                               TO PAGE-CTR.
+           MOVE PAGE-CTR                       TO TITLE-PAGE.
            WRITE PRTLINE FROM TITLE-LINE1.
            WRITE PRTLINE FROM TITLE-LINE2
                AFTER ADVANCING 1 LINE.
@@ -334,8 +324,8 @@
                AFTER ADVANCING 2 LINES.
 
       *ERROR REPORT HEADINGS		 
-	       ADD 1 TO ERR-PAGE-CTR.
-           MOVE ERR-PAGE-CTR TO ERR-TITLE-PAGE.
+	       ADD 1                               TO ERR-PAGE-CTR.
+           MOVE ERR-PAGE-CTR                   TO ERR-TITLE-PAGE.
            WRITE ERRLINE FROM ERR-TITLE-LINE.
            WRITE ERRLINE FROM TITLE-LINE2
                AFTER ADVANCING 1 LINE.
@@ -345,137 +335,104 @@
 		       AFTER ADVANCING 2 LINES.
 	
        L3-VALIDATION.
-	       MOVE 'N' TO ERR-SW.
+	       MOVE 'N'                            TO ERR-SW.
 	           IF I-LNAME EQUAL SPACES
-		           MOVE 'LAST NAME REQUIRED' TO ERR-MSG
-		           MOVE 'Y' TO ERR-SW
-		           GO TO L3-VALIDATION-EXIT.
+		           MOVE ERROR-TABLE-MSG(1)     TO ERR-MSG
+		           MOVE 'Y'                    TO ERR-SW
+		           GO TO L9-VALIDATION-EXIT.
 
-			   IF I-FNAME EQUAL SPACES
-		           MOVE 'FIRST NAME REQUIRED' TO ERR-MSG
-		           MOVE 'Y' TO ERR-SW
-		           GO TO L3-VALIDATION-EXIT.
+			   IF I-FNAME EQUAL SPACES 
+		           MOVE ERROR-TABLE-MSG(2)     TO ERR-MSG
+		           MOVE 'Y'                    TO ERR-SW
+		           GO TO L9-VALIDATION-EXIT.
 
 			   IF I-ADDRESS EQUAL SPACES
-		           MOVE 'ADDRESS REQUIRED' TO ERR-MSG
-		           MOVE 'Y' TO ERR-SW
-		           GO TO L3-VALIDATION-EXIT.
+		           MOVE ERROR-TABLE-MSG(3)     TO ERR-MSG
+		           MOVE 'Y'                    TO ERR-SW
+		           GO TO L9-VALIDATION-EXIT.
 
 			   IF I-CITY EQUAL SPACES
-		           MOVE 'CITY REQUIRED' TO ERR-MSG
-		           MOVE 'Y' TO ERR-SW
-		           GO TO L3-VALIDATION-EXIT.
+		           MOVE ERROR-TABLE-MSG(4)     TO ERR-MSG
+		           MOVE 'Y'                    TO ERR-SW
+		           GO TO L9-VALIDATION-EXIT.
 
 	           IF NOT VAL-STATES
-		           MOVE 'STATES MUST BE IA, IL, MI, MO, NE, OR WI'
-                   TO ERR-MSG
-		           MOVE 'Y' TO ERR-SW
-		           GO TO L3-VALIDATION-EXIT.
+		           MOVE ERROR-TABLE-MSG(5)     TO ERR-MSG
+		           MOVE 'Y'                    TO ERR-SW
+		           GO TO L9-VALIDATION-EXIT.
 
 	           IF I-ZIP NOT NUMERIC
-		           MOVE 'ZIP CODE MUST BE NUMERIC' TO ERR-MSG
-		           MOVE 'Y' TO ERR-SW
-	               GO TO L3-VALIDATION-EXIT.
+		           MOVE ERROR-TABLE-MSG(6)     TO ERR-MSG
+		           MOVE 'Y'                    TO ERR-SW
+	               GO TO L9-VALIDATION-EXIT.
 
 	           IF I-POP-TYPE NUMERIC
 		           IF VAL-POP-TYPES
 			           NEXT SENTENCE
 		           ELSE
-			           MOVE 'POP TYPE MUST BE 1-6' TO ERR-MSG
-			           MOVE 'Y' TO ERR-SW
-			           GO TO L3-VALIDATION-EXIT
+			           MOVE ERROR-TABLE-MSG(7) TO ERR-MSG
+			           MOVE 'Y'                TO ERR-SW
+			           GO TO L9-VALIDATION-EXIT
 		           END-IF
 	           ELSE
-		           MOVE 'POP TYPE MUST BE NUMERIC' TO ERR-MSG
-			       MOVE 'Y' TO ERR-SW
-			       GO TO L3-VALIDATION-EXIT
+		           MOVE ERROR-TABLE-MSG(8)     TO ERR-MSG
+			       MOVE 'Y'                    TO ERR-SW
+			       GO TO L9-VALIDATION-EXIT
 			   END-IF.
 
                IF I-CASES NUMERIC
 		           IF I-CASES >= 1
 			           NEXT SENTENCE
 		           ELSE
-			           MOVE 'MINIMUM OF 1 CASE' TO ERR-MSG
-			           MOVE 'Y' TO ERR-SW
-			           GO TO L3-VALIDATION-EXIT
+			           MOVE ERROR-TABLE-MSG(9) TO ERR-MSG
+			           MOVE 'Y'                TO ERR-SW
+			           GO TO L9-VALIDATION-EXIT
 		           END-IF
 	           ELSE
-		           MOVE 'NUMBER OF CASES MUST BE NUMERIC' TO ERR-MSG
-			       MOVE 'Y' TO ERR-SW
-			       GO TO L3-VALIDATION-EXIT
+		           MOVE ERROR-TABLE-MSG(10)    TO ERR-MSG
+			       MOVE 'Y'                    TO ERR-SW
+			       GO TO L9-VALIDATION-EXIT
 		       END-IF.
 
 			   IF NOT VAL-TEAMS
-				   MOVE 'TEAM MUST BE A, B, C, D, OR E' TO ERR-MSG
-				   MOVE 'Y' TO ERR-SW
-				   GO TO L3-VALIDATION-EXIT
+				   MOVE ERROR-TABLE-MSG(11)    TO ERR-MSG
+				   MOVE 'Y'                    TO ERR-SW
+				   GO TO L9-VALIDATION-EXIT
                END-IF.
-			
-       L3-VALIDATION-EXIT.
-	       EXIT.
 	
        L3-ERROR-PRINT.
 	       COMPUTE C-GT-ERR-CTR = C-GT-ERR-CTR + 1.
-	       MOVE I-REC TO ERR-REC
+	       MOVE I-REC                          TO ERR-REC
 	       WRITE ERRLINE FROM ERROR-LINE
 		       AFTER ADVANCING 2 LINES
 			       AT EOP
 				       PERFORM L4-ERROR-HEADING.
        L3-CALCS.
-	       EVALUATE I-STATE
-		       WHEN 'IA'
-			       MOVE 0.05 TO C-DEP
-		       WHEN 'IL'
-			       MOVE 0 TO C-DEP
-		       WHEN 'MI'
-			       MOVE 0.10 TO C-DEP
-		       WHEN 'MO'
-			       MOVE 0 TO C-DEP
-		       WHEN 'NE'
-			       MOVE 0.05 TO C-DEP
-		       WHEN 'WI'
-			       MOVE 0.05 TO C-DEP
-	       END-EVALUATE.
+	       PERFORM
+			   VARYING SUB FROM 1 BY 1
+				   UNTIL I-STATE = DEP-STATE(SUB)
+           END-PERFORM.
+
+		   MOVE DEP-AMNT(SUB) TO C-DEP
 	
 	       COMPUTE C-TOT-DEP = C-DEP * (24 * I-CASES).
 	       COMPUTE C-TOT-SALES = (18.71 * I-CASES) + C-TOT-DEP.
-	
-	       EVALUATE I-POP-TYPE
-		       WHEN 1
-			       COMPUTE C-GT-COKE-CASES = C-GT-COKE-CASES + I-CASES
-			       MOVE 'COKE' TO D-POP-TYPE
-		       WHEN 2
-			       COMPUTE C-GT-DC-CASES = C-GT-DC-CASES + I-CASES
-			       MOVE  'DIET COKE' TO D-POP-TYPE
-		       WHEN 3
-			       COMPUTE C-GT-MY-CASES = C-GT-MY-CASES + I-CASES
-			       MOVE 'MELLO YELLO' TO D-POP-TYPE
-		       WHEN 4
-			       COMPUTE C-GT-CC-CASES = C-GT-CC-CASES + I-CASES
-			       MOVE 'CHERRY COKE' TO D-POP-TYPE
-		       WHEN 5
-			       COMPUTE C-GT-DCC-CASES = C-GT-DCC-CASES + I-CASES
-			       MOVE 'DIET CHERRY COKE' TO D-POP-TYPE
-		       WHEN 6
-			       COMPUTE C-GT-SPRITE-CASES = C-GT-SPRITE-CASES
-                                               + I-CASES
-			       MOVE 'SPRITE' TO D-POP-TYPE
-	       END-EVALUATE.
-	
-	       EVALUATE I-TEAM
-		       WHEN 'A'
-			       COMPUTE C-GT-A-TEAM = C-GT-A-TEAM + C-TOT-SALES
-		       WHEN 'B'
-			       COMPUTE C-GT-B-TEAM = C-GT-B-TEAM + C-TOT-SALES
-		       WHEN 'C'
-			       COMPUTE C-GT-C-TEAM = C-GT-C-TEAM + C-TOT-SALES
-		       WHEN 'D'
-			       COMPUTE C-GT-D-TEAM = C-GT-D-TEAM + C-TOT-SALES
-		       WHEN 'E'
-			       COMPUTE C-GT-E-TEAM = C-GT-E-TEAM + C-TOT-SALES
-	       END-EVALUATE.
+	       
+		   PERFORM
+			   VARYING SUB FROM 1 BY 1
+				   UNTIL SUB = I-POP-TYPE
+           END-PERFORM.
+
+		   COMPUTE C-POP-TOTAL(SUB) = C-POP-TOTAL(SUB) + I-CASES.
+		   MOVE POP-LIT(SUB) TO D-POP-TYPE.
+
+		   PERFORM
+			   VARYING SUB FROM 1 BY 1
+				   UNTIL I-TEAM = TEAM-CODE(SUB)
+           END-PERFORM.
+
+		   COMPUTE TEAM-TOTAL(SUB) = TEAM-TOTAL(SUB) + C-TOT-SALES.
 		
-	
        L3-MOVE-PRINT.
            MOVE I-LNAME        TO D-LNAME.
 		   MOVE I-FNAME        TO D-FNAME.
@@ -508,46 +465,31 @@
 		       AFTER ADVANCING 1 LINE.
            WRITE PRTLINE FROM GT-POP-LINE
 		       AFTER ADVANCING 2 LINES.
-		   MOVE 'COKE' TO GT-POP-NAME1.
-           MOVE C-GT-COKE-CASES TO GT-POP-TOTAL1.
-		   MOVE 'DIET COKE' TO GT-POP-NAME2.
-           MOVE C-GT-DC-CASES TO GT-POP-TOTAL2.
-		   MOVE 'MELLO YELLO' TO GT-POP-NAME3
-           MOVE C-GT-MY-CASES TO GT-POP-TOTAL3.
-           WRITE PRTLINE FROM GT-POP-LINE2
-               AFTER ADVANCING 2 LINES.
-           MOVE 'CHERRY COKE' TO GT-POP-NAME1.
-           MOVE C-GT-CC-CASES TO GT-POP-TOTAL1.
-		   MOVE 'DIET CHERRY COKE' TO GT-POP-NAME2.
-           MOVE C-GT-DCC-CASES TO GT-POP-TOTAL2.
-		   MOVE 'SPRITE' TO GT-POP-NAME3
-           MOVE C-GT-SPRITE-CASES TO GT-POP-TOTAL3.
-	       WRITE PRTLINE FROM GT-POP-LINE2
-               AFTER ADVANCING 2 LINES.
-		
-	       WRITE PRTLINE FROM GT-TEAM-LINE
-		       AFTER ADVANCING 3 LINES.
-		   MOVE 'A' TO GT-TEAM-NAME.
-		   MOVE C-GT-A-TEAM TO GT-TEAM-TOTAL.
-	       WRITE PRTLINE FROM GT-TEAM-LINE2
-		       AFTER ADVANCING 2 LINES.
-		   MOVE 'B' TO GT-TEAM-NAME.
-		   MOVE C-GT-B-TEAM TO GT-TEAM-TOTAL.
-	       WRITE PRTLINE FROM GT-TEAM-LINE2
-		       AFTER ADVANCING 2 LINES.
-		   MOVE 'C' TO GT-TEAM-NAME.
-		   MOVE C-GT-C-TEAM TO GT-TEAM-TOTAL.
-	       WRITE PRTLINE FROM GT-TEAM-LINE2
-		       AFTER ADVANCING 2 LINES.
-		   MOVE 'D' TO GT-TEAM-NAME.
-		   MOVE C-GT-D-TEAM TO GT-TEAM-TOTAL.
-	       WRITE PRTLINE FROM GT-TEAM-LINE2
-		       AFTER ADVANCING 2 LINES.
-		   MOVE 'E' TO GT-TEAM-NAME.
-		   MOVE C-GT-E-TEAM TO GT-TEAM-TOTAL.
-	       WRITE PRTLINE FROM GT-TEAM-LINE2
-		       AFTER ADVANCING 2 LINES.
 
+		   PERFORM
+			   VARYING SUB FROM 1 BY 1
+				   UNTIL SUB > 3
+					   MOVE POP-LIT(SUB) TO GT-POP-LITERAL(SUB)
+					   MOVE C-POP-TOTAL(SUB) TO GT-POP-TOTAL(SUB) 
+           END-PERFORM.
+
+           WRITE PRTLINE FROM GT-POP-LINE2.
+
+		   PERFORM
+			   VARYING SUB FROM 1 BY 1
+				   UNTIL SUB > 3
+					   MOVE POP-LIT(SUB + 3) TO GT-POP-LITERAL(SUB)
+					   MOVE C-POP-TOTAL(SUB + 3) TO GT-POP-TOTAL(SUB) 
+           END-PERFORM.
+
+		   WRITE PRTLINE FROM GT-POP-LINE2.
+
+		   PERFORM
+			   VARYING SUB FROM 1 BY 1
+				   UNTIL SUB > 5
+					   MOVE TEAM-CODE(SUB) TO GT-TEAM-NAME(SUB)
+					   MOVE TEAM-TOTAL(SUB) TO GT-TEAM-TOTAL(SUB) 
+           END-PERFORM.
 
        L4-HEADING.
            ADD 1 TO PAGE-CTR.
@@ -573,6 +515,9 @@
 	       WRITE ERRLINE FROM ERR-COL-HEADING
 		       AFTER ADVANCING 2 LINES.
  
+       L9-VALIDATION-EXIT.
+	       EXIT.
+
        L9-READ-INPUT.
            READ POP-INPUT
                AT END
